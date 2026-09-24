c5a2c3cacb933cb9521b4781f17cfc3bdab185b5
Tanisha Vyas <165962059+tanishavyastv@users.noreply.github.com>
2026-09-24 10:53:11 +0530

    api base url changed

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
+  baseURL:
+    import.meta.env.VITE_API_BASE_URL ||
+    "https://new-finance-back.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
