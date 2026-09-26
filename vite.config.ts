8da7ddca03a5e991feeca804330a8b61b46a38c7
Tanisha Vyas <165962059+tanishavyastv@users.noreply.github.com>
2026-09-26 12:55:25 +0530

    Vendor chunk splitting

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,17 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss()
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react-router') || /node_modules\/react(\/|-dom)/.test(id)) return 'vendor-react';
+            if (id.includes('i18next')) return 'vendor-i18n';
+            if (id.includes('axios')) return 'vendor-api';
+          }
+        },
+      },
+    },
+  },
 })
